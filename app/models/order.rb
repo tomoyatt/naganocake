1bda39bbc9e2f1a8f983d1407afcb2cf89ad01fb
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-06-07 13:43:09 +0000

    [Add] admin/items/new

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,16 @@
 class Order < ApplicationRecord
   has_many :order_details
   belongs_to :customer
+  
+  enum payment_method: {
+    credit_card: 0, transfer: 1
+  }
+  
+  enum status: {
+    awaiting_payment: 0,
+    payment_confirmationunder: 1,
+    in_production: 2,
+    shipping_preparation: 3,
+    shipped: 4
+  }
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,11 @@
 class OrderDetail < ApplicationRecord
   belongs_to :item
   belongs_to :order
+  
+  enum making_status: {
+    non_production: 0,
+    awaiting_production: 1,
+    in_production: 2,
+    production_complete: 3
+  }
 end
