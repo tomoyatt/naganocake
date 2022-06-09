14d10dbf9b69dcad4b85415caf1b3264742b664a
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-06-09 14:06:14 +0000

    [Add] header

--- config/routes.rb
+++ config/routes.rb
@@ -9,23 +9,17 @@ Rails.application.routes.draw do
     root to: "homes#top"
     get 'about' => "homes#about"
     resources :items,       only: [:index, :show]
-    get 'customers/my_page' => "customers#show"
-    get 'customers/edit' => "customers#edit"
-    patch 'customers' => "customers#update"
-    get 'customers/unsubscribe' => "customers#unsubscribe"
-    patch 'customers/withdraw' => "customers#withdraw"
-    get 'cart_items' => "cart_items#index"
-    patch 'cart_items/:id' => "cart_items#update"
-    delete 'cart_items/:id' => "cart_items#destroy"
-    delete 'cart_items/destroy_all' => "cart_items#destroy_all"
-    post 'cart_items' => "cart_items#create"
-    get 'orders/new' => "orders#new"
-    post 'orders/confirm' => "orders#confirm"
-    get 'orders/conplete' => "orders#conplete"
-    post 'orders' => "orders#create"
-    get 'orders' => "orders#index"
-    get 'orders/:id' => "orders#show"
+    resources :cart_items,  only: [:index, :update, :destroy, :create]
+    resources :orders,      only: [:new, :create, :index, :show]
     resources :addresses,   only: [:index, :edit, :create, :update, :destroy]
+    get 'customers/my_page'         => "customers#show"
+    get 'customers/edit'            => "customers#edit"
+    patch 'customers'               => "customers#update"
+    get 'customers/unsubscribe'     => "customers#unsubscribe"
+    patch 'customers/withdraw'      => "customers#withdraw"
+    delete 'cart_items/destroy_all' => "cart_items#destroy_all"
+    post 'orders/confirm'           => "orders#confirm"
+    get 'orders/conplete'           => "orders#conplete"
   end
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
