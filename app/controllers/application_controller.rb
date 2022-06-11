8e16194ad2be1d5f2b2a34dfba33f1c2b429cbd6
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-06-11 13:16:38 +0000

    [Add] is_deleted

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_admin!, if: :admin_url
   
   protected
   
@@ -7,5 +8,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [
       :last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number
     ])
+  end
+  
+  def admin_url
+    request.fullpath.include?("/admin")
   end
 end
