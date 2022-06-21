4643f72ea10092efc30cc20ed3648c5013c374a6
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-06-21 14:42:22 +0000

    [edit] confirm

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -3,56 +3,58 @@ class Public::OrdersController < ApplicationController
     @order = Order.new
   end
   
-  def create
+  def confirm
     customer = current_customer
-    session[:order] = Order.new
-    cart_items = current_customer.cart_items
-    
-    sum = 0
-    cart_items.each do |cart_item|
-      sum += (cart_item.item.with_tax_price) * cart_item.amount
+    params[:order][:payment_method] = params[:order][:payment_method].to_i
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items
+    destination = params[:order][:address_number]
+    @total_price = 0
+    @cart_items.each do |cart_item|
+      @total_price += (cart_item.item.price * 1.1 * cart_item.amount).floor
     end
     
-    session[:order][:shipping_cost] = 800
-    session[:order][:total_payment] = sum + session[:order][:shipping_cost]
-    session[:order][:status] = 0
-    session[:order][:customer_id] = current_customer.id
-    session[:order][:payment_method] = params[:payment_method].to_i
-    
-    destination = params[:address_number].to_i
     if destination == 0
-      session[:order][:postal_code] = customer.postal_code
-      session[:order][:address] = customer.address
-      session[:order][:name] = customer.last_name + customer.first_name
+      @order.postal_code = customer.postal_code
+      @order.address = customer.address
+      @order.name = customer.last_name + customer.first_name
     elsif destination == 1
-      address = Address.find(params[:address_id])
-      session[:order][:postal_code] = address.postal_code
-      session[:order][:address] = address.address
-      session[:order][:name] = address.name
+      address = Address.find(params[:order][:address])
+      @order.postal_code = address.postal_code
+      @order.address = address.address
+      @order.name = address.name
     elsif destination == 2
-      session[:new_address] = 2
-      session[:order][:postal_code] = params[:postal_code]
-      session[:order][:address] = params[:address]
-      session[:order][:name] = params[:name]
+      @address = Address.new()
+      @address.postal_code = params[:order][:postal_code]
+      @address.address = params[:order][:address]
+      @address.name = params[:order][:name]
+      @address.customer_id = customer.id
+      if @address.save
+        @order.postal_code = @address.postal_code
+        @order.address = @address.address
+        @order.name = @address.name
+      else
+        render :new
+      end
     end
-    
-    if session[:order][:postal_code].presence && session[:order][:address].presence && session[:order][:name].presence
-      redirect_to orders_confirm_path
-    else
-      redirect_to new_order_path
-    end
-      
   end
   
-  def confirm
+  def create
   end
   
   def complete
   end
 
   def index
+    @orders = current_customer.orders
   end
 
   def show
+  end
+  
+  private
+  
+  def order_params
+    params.permit(:payment_method, :address, :shipping_cost, :postal_code, :name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,18 +8,19 @@ Rails.application.routes.draw do
   scope module: :public do
     root to: "homes#top"
     get 'about' => "homes#about"
-    resources :items,       only: [:index, :show]
-    resources :cart_items,  only: [:index, :update, :destroy, :create]
-    resources :orders,      only: [:new, :create, :index, :show]
-    resources :addresses,   only: [:index, :edit, :create, :update, :destroy]
     get 'customers/my_page'         => "customers#show"
     get 'customers/edit'            => "customers#edit"
     patch 'customers'               => "customers#update"
     get 'customers/unsubscribe'     => "customers#unsubscribe"
     patch 'customers/withdraw'      => "customers#withdraw"
     delete 'cart_items'             => "cart_items#destroy_all", as: "cart_items_destroy_all"
+    get 'orders/confirm'            => "orders#confirm"
     post 'orders/confirm'           => "orders#confirm"
     get 'orders/conplete'           => "orders#conplete"
+    resources :items,       only: [:index, :show]
+    resources :cart_items,  only: [:index, :update, :destroy, :create]
+    resources :orders,      only: [:new, :create, :index, :show]
+    resources :addresses,   only: [:index, :edit, :create, :update, :destroy]
   end
   
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
