843f9fed30558d09aa634a104166548862cf204b
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-06-30 09:03:08 +0000

    [edit] header

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,10 +1,12 @@
 class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
+    @genres = Genre.all
   end
 
   def show
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @genres = Genre.all
   end
 end
