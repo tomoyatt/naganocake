ca686657f42edbafb5c551d1242df6e11d22d8a4
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-07-16 06:25:22 +0000

    [edit] layout public/items.genres

--- app/controllers/public/genres_controller.rb
+++ app/controllers/public/genres_controller.rb
@@ -2,6 +2,6 @@ class Public::GenresController < ApplicationController
   def show
     @genre = Genre.find(params[:id])
     @genres = Genre.all
-    @items = Item.where(genre_id: @genre.id)
+    @items = Item.where(genre_id: @genre.id).page(params[:page]).per(8)
   end
 end
