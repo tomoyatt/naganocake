05ff441d39ee61f60dc16afb4811b9490a02c4ad
tomoyatt <104720184+tomoyatt@users.noreply.github.com>
2022-07-19 14:20:04 +0000

    [edit] layout -> next table border

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -4,7 +4,7 @@ class Public::OrdersController < ApplicationController
   
   def new
     @order = Order.new
-    @order.customer_id = current_customer.id
+    @address = Address.where(customer_id: current_customer.id)
   end
   
   def confirm
